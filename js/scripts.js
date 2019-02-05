4dee0944ecc368399046c56213666d805086b4e0
Jimmy Zebroski <zebrozkii@gmail.com>
2019-02-05 14:38:17 -0800

    working on price check functions using toppings

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,47 @@
-function Pizza(sizeInput,pizzaToppings,pizzaPrice){
-  this.sizeInput=sizeInput,
-  this.pizzaToppings=pizzaToppings,
-  this.pizzaPrice=pizzaPrice
+$(document).ready(function(){
+function Pizza(sizeInput,toppingArr,pizzaPrice){
+  this.sizeInput=sizeInput;
+  this.toppingArr=toppingArr;
+  this.pizzaPrice=pizzaPrice;
 }
 Pizza.prototype.sizeCheck=function(){
   if (this.sizeInput==="small") {
     this.pizzaPrice+=10;
+console.log(this.pizzaPrice);
   }
-  if(this.sizeInput==="medium"){
+  else if(this.sizeInput==="medium"){
     this.pizzaPrice+=12;
   }
-  if (this.sizeInput==="large") {
+  else{
     this.pizzaPrice+=14;
   }
-  alert(this.pizzaPrice);
+
+};
+Pizza.prototype.priceCheck=function(){
+  for (var i = 0; i < this.toppingArr.length; i++) {
+    if (this.toppingArr.length===0) {
+      this.pizzaPrice=this.pizzaPrice;
+    }else if (this.toppingArr.length===1) {
+      this.pizzaPrice+=2;
+    }else {
+      this.pizzaPrice+=4;
+    }
+  }
+  console.log(this.pizzaPrice);
 };
 
-
-
-
-$(document).ready(function(){
   $("form#formOne").submit(function(event){
     event.preventDefault();
     var sizeInput=$("input[name=size]:checked").val();
   //  alert(sizeInput);
-    var pizzaToppings=$("input[name=toping]:checked").val();
-  //  alert(pizzaToppings);
+  var toppingArr=[];
+  $("input[name=topping]:checked").each(function(){
+      toppingArr.push($(this).val());
+    });
+   alert(toppingArr);
     var pizzaPrice=0;
-    var thePizza=new Pizza(sizeInput,pizzaToppings,pizzaPrice);
-
+    var thePizza=new Pizza(sizeInput,toppingArr,pizzaPrice);
+    thePizza.sizeCheck();
+    thePizza.priceCheck();
 });
 });
